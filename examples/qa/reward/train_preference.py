755ceed96f041790299e6ee8075a82fbacaf495c
Lukas Fluri <lukas.fluri@protonmail.com>
2024-12-12 01:38:12 -0800

    Adding adapted code to train a reward model on general arena labels.

--- examples/qa/reward/train_preference.py
+++ examples/qa/reward/train_preference.py
@@ -1,30 +1,36 @@
+import argparse
+import json
 import os
+from pathlib import Path
 
+import numpy as np
+import pytorch_lightning as pl
 import torch
 from datasets import load_dataset
-from reward_model import GPTRewardModel
+from peft import LoraConfig
+from reward_model import GPTRewardModel, GPTRewardModelLora
 from torch.utils.data import Dataset
 from tqdm import tqdm
 from transformers import AutoTokenizer, Trainer, TrainingArguments
-import json
-import pytorch_lightning as pl
-import argparse
-import numpy as np
+
 
 def create_comparison_dataset(path):
     def get_prompt(conversation):
-        res = ''
+        res = ""
         for utt in conversation:
-            res += f"{utt['role']}: {utt['content']}"
+            res += f"{utt['role']}: {utt['content']}\n"
         return res
-    
-    with open(path, 'r') as f:
-        dataset = json.load(f)
+
+    dataset = []
+    with open(path, "r") as f:
+        for line in f:
+            sample = json.loads(line)
+            dataset.append(sample)
     pairs = []
     for sample in tqdm(dataset):
         pair = {}
-        win_response = get_prompt(sample['win'])
-        lose_response = get_prompt(sample['lose'])
+        win_response = get_prompt(sample["win"])
+        lose_response = get_prompt(sample["lose"])
         if win_response == lose_response:
             continue
         if len(win_response.split()) < 5 or len(lose_response.split()) < 5:
@@ -36,11 +42,23 @@ def create_comparison_dataset(path):
 
 
 class PairwiseDataset(Dataset):
-    def __init__(self, pairs, tokenizer, max_length):
+    def __init__(self, name, pairs, tokenizer, max_length):
         self.chosen_input_ids = []
         self.chosen_attn_masks = []
         self.rejected_input_ids = []
         self.rejected_attn_masks = []
+
+        # Check if cache files exist
+        if Path(f"cache/chosen_input_ids_{name}.pt").is_file():
+            self.chosen_input_ids = torch.load(f"cache/chosen_input_ids_{name}.pt")
+            self.chosen_attn_masks = torch.load(f"cache/chosen_attn_masks_{name}.pt")
+            self.rejected_input_ids = torch.load(f"cache/rejected_input_ids_{name}.pt")
+            self.rejected_attn_masks = torch.load(f"cache/rejected_attn_masks_{name}.pt")
+
+            print(f"raw size = {len(pairs)}, encoded size = {len(self.chosen_input_ids)}")
+
+            return
+
         for pair in tqdm(pairs):
             chosen, rejected = pair["chosen"], pair["rejected"]
             chosen_encodings_dict = tokenizer(
@@ -64,9 +82,22 @@ class PairwiseDataset(Dataset):
             self.rejected_input_ids.append(rejected_encodings_dict["input_ids"])
             self.rejected_attn_masks.append(rejected_encodings_dict["attention_mask"])
         print(f"raw size = {len(pairs)}, encoded size = {len(self.chosen_input_ids)}")
+
+        # Store the lists as PyTorch tensors
+        chosen_input_ids = torch.stack(self.chosen_input_ids)
+        chosen_attn_masks = torch.stack(self.chosen_attn_masks)
+        rejected_input_ids = torch.stack(self.rejected_input_ids)
+        rejected_attn_masks = torch.stack(self.rejected_attn_masks)
+
+        # Store the lists in files
+        torch.save(chosen_input_ids, f"chosen_input_ids_{name}.pt")
+        torch.save(chosen_attn_masks, f"chosen_attn_masks_{name}.pt")
+        torch.save(rejected_input_ids, f"rejected_input_ids_{name}.pt")
+        torch.save(rejected_attn_masks, f"rejected_attn_masks_{name}.pt")
+
     def __len__(self):
         return len(self.chosen_input_ids)
-    
+
     def show_example(self):
         print(tokenizer.decode(self.chosen_input_ids[0][0]))
 
@@ -103,28 +134,29 @@ def get_args():
     parser = argparse.ArgumentParser()
     # for distributed launcher
     parser.add_argument("--local_rank", type=int, default=0)
-    
+
     parser.add_argument("--ckpt_path", type=str)
     parser.add_argument("--tokenizer_path", type=str)
-    
+
     parser.add_argument("--run_name", type=str)
-    
+
     parser.add_argument("--seed", type=int, default=42)
     parser.add_argument("--train_data", type=str)
     parser.add_argument("--val_data", type=str)
-    parser.add_argument("--output_dir", type=str, help='checkpoint save path')
-    parser.add_argument("--logging_dir", type=str, help='log save path')
-    
+    parser.add_argument("--output_dir", type=str, help="checkpoint save path")
+    parser.add_argument("--logging_dir", type=str, help="log save path")
+
     parser.add_argument("--max_len", type=int, default=2048)
-    
+
     parser.add_argument("--lr", type=float, default=1e-5)
     parser.add_argument("--batch_size", type=int, default=32)
     parser.add_argument("--max_epochs", type=int, default=5)
     parser.add_argument("--eval_steps", type=int, default=None)
-    parser.add_argument('--save_steps', type=int, default=None)
+    parser.add_argument("--save_steps", type=int, default=None)
     parser.add_argument("--gradient_accumulation", type=int, default=1)
-    parser.add_argument("--flash_attn", action='store_true')
+    parser.add_argument("--flash_attn", action="store_true")
     parser.add_argument("--deepspeed_config", type=str)
+    parser.add_argument("--use_lora", action="store_true")
     args = parser.parse_args()
     return args
 
@@ -132,20 +164,32 @@ def get_args():
 if __name__ == "__main__":
     args = get_args()
     pl.seed_everything(args.seed)
-    
+
     tokenizer = AutoTokenizer.from_pretrained(args.tokenizer_path)
-    tokenizer.padding_side = 'right'
-    
-    if tokenizer.pad_token is None:
-        tokenizer.pad_token = tokenizer.unk_token
-        tokenizer.pad_token_id = tokenizer.unk_token_id
-    print('tokenizer pad token = ', tokenizer.pad_token)
-    
+    tokenizer.padding_side = "right"
+
+    if "Llama-2-" in args.tokenizer_path:
+        if tokenizer.pad_token is None:
+            tokenizer.pad_token = tokenizer.unk_token
+            tokenizer.pad_token_id = tokenizer.unk_token_id
+    elif "Llama-3-" in args.tokenizer_path:
+        if tokenizer.pad_token is None:
+            tokenizer.pad_token = tokenizer.eos_token
+            tokenizer.pad_token_id = tokenizer.eos_token_id
+    print("tokenizer pad token = ", tokenizer.pad_token)
+
+    lora_config = LoraConfig(
+        r=16,
+        lora_alpha=16,
+        layers_to_transform=list(range(22, 32)),
+        lora_dropout=0.1,
+    )
+
     training_args = TrainingArguments(
         output_dir=args.output_dir,
         logging_dir=args.logging_dir,
         evaluation_strategy="epoch" if args.eval_steps is None else "steps",
-        save_strategy='epoch' if args.save_steps is None else "steps",
+        save_strategy="epoch" if args.save_steps is None else "steps",
         eval_accumulation_steps=1,
         learning_rate=args.lr,
         per_device_train_batch_size=args.batch_size,
@@ -157,14 +201,14 @@ if __name__ == "__main__":
         gradient_accumulation_steps=args.gradient_accumulation,
         num_train_epochs=args.max_epochs,
         save_only_model=True,
-        lr_scheduler_type='linear',
+        lr_scheduler_type="linear",
         eval_steps=args.eval_steps,
         save_steps=args.save_steps,
         logging_steps=args.eval_steps,
         remove_unused_columns=False,
         deepspeed=args.deepspeed_config,
         run_name=args.run_name,
-        metric_for_best_model='eval_loss',
+        metric_for_best_model="eval_loss",
     )
 
     # Create the comparisons datasets
@@ -172,21 +216,20 @@ if __name__ == "__main__":
     val_pairs = create_comparison_dataset(args.val_data)
 
     # Make pairwise datasets for training
-    train_dataset = PairwiseDataset(train_pairs, tokenizer, max_length=args.max_len)
-    val_dataset = PairwiseDataset(val_pairs, tokenizer, max_length=args.max_len)
+    train_dataset = PairwiseDataset("train1", train_pairs, tokenizer, max_length=args.max_len)
+    val_dataset = PairwiseDataset("val1", val_pairs, tokenizer, max_length=args.max_len)
 
     # Create the collator to gather batches of pairwise comparisons
     data_collator = DataCollatorReward()
-    
-    # Initialize the reward model from the (supervised) fine-tuned GPT-J
-    model = GPTRewardModel(args.ckpt_path, tokenizer_path=args.tokenizer_path)
 
-    # Freeze the first 70% of the hidden layers of the reward model backbone
-    layers = model.transformer.layers
-    num_layers = len(layers)
-    num_unfrozen = int(0.3 * num_layers)
-    for layer in layers[:-num_unfrozen]:
-        layer.requires_grad_(False)
+    # Initialize the reward model from the (supervised) fine-tuned GPT-J
+    if args.use_lora:
+        model = GPTRewardModelLora(args.ckpt_path, tokenizer_path=args.tokenizer_path, lora_config=lora_config)
+    else:
+        model = GPTRewardModel(args.ckpt_path, tokenizer_path=args.tokenizer_path)
+
+    # Print some statistics about the model
+    model.print_trainable_parameters()
 
     trainer = Trainer(
         model=model,
@@ -196,5 +239,7 @@ if __name__ == "__main__":
         compute_metrics=compute_metrics,
         data_collator=data_collator,
     )
-    
+
+    print("Starting training...")
+
     trainer.train()
--- examples/qa/reward/train_preference.sh
+++ examples/qa/reward/train_preference.sh
@@ -1,9 +1,28 @@
+#!/bin/bash
+#SBATCH --output=outputs/slurm-%j.out
+#SBATCH --cpus-per-task=16
+#SBATCH --mem="512gb"
+#SBATCH --gpus=A100-SXM4-80GB:4
+#SBATCH --time="23:59:59"
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --qos="high"
+#SBATCH --mail-type=END,FAIL
+
+
+#TEMP: Maybe remove this: SBATCH --nodelist="cirl.ist.berkeley.edu,rlhf.ist.berkeley.edu,airl.ist.berkeley.edu,sac.ist.berkeley.edu"
+
+export NCCL_P2P_LEVEL=NVL
+conda activate mislead
+echo "Conda environment: $CONDA_DEFAULT_ENV"
+
 MAX_EPOCH=5
 
 CKPT_NAME=Llama-2-13b-hf
 
 LR=1e-5
-DEEPSPEED=../ds_configs/ds_config_zero2.json
+# DEEPSPEED=../ds_configs/ds_config_zero2.json
+DEEPSPEED=../ds_configs/ds_config_zero2_custom.json
 BC=4
 GRAD_ACC=1
 
@@ -11,17 +30,17 @@ let GLOBAL_BATCH_SIZE=8*$BC*$GRAD_ACC
 echo "global batch size = "$GLOBAL_BATCH_SIZE
 
 DATA_NAME=arena-55k
-TRAIN_DATA=data/$DATA_NAME/train.json
-VAL_DATA=/data/$DATA_NAME/test.json
+TRAIN_DATA=../data/$DATA_NAME/train_openai.json
+VAL_DATA=../data/$DATA_NAME/test_openai.json
 
 EXP_NAME=lr${LR}_bc${GLOBAL_BATCH_SIZE}_maxepoch${MAX_EPOCH}_full_fixsep
 
 
-SAVE_DIR=XXX
+SAVE_DIR=outputs
 
 LOGGING_DIR=results/$CKPT_NAME/$EXP_NAME
 
-deepspeed train_preference.py \
+deepspeed --num_gpus 4 --master_port 6601 train_preference.py \
     --run_name $EXP_NAME \
     --deepspeed_config $DEEPSPEED \
     --train_data $TRAIN_DATA \
@@ -32,8 +51,8 @@ deepspeed train_preference.py \
     --lr $LR \
     --batch_size $BC \
     --max_epochs $MAX_EPOCH \
-    --ckpt_path XXX \
-    --tokenizer_path XXX \
+    --ckpt_path meta-llama/Llama-2-13b-hf \
+    --tokenizer_path meta-llama/Llama-2-13b-hf \
     --gradient_accumulation $GRAD_ACC \
     --flash_attn \
     --eval_steps 50 \
