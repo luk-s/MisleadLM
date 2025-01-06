6aa5efa6559b7a143f3abd59e10a363d41b33777
Lukas Fluri <lukas.fluri@protonmail.com>
2025-01-06 01:45:02 -0800

    Cleaning up and adding reward model selection to the reward model inference logic.

--- examples/qa/reward_api.py
+++ examples/qa/reward_api.py
@@ -1,37 +1,46 @@
-import json
 import argparse
-from typing import List
-import torch
-from reward.reward_model import GPTRewardModel
-from transformers import AutoTokenizer
+import json
 import pathlib
-from trlx.data.configs import TRLConfig
-from tqdm import tqdm
+from typing import List
+
 import safetensors.torch
+import torch
+from flask import Flask, jsonify, make_response, request
+from reward.reward_model import GPTRewardModel
+from tqdm import tqdm
+from transformers import AutoTokenizer
 
-from flask import Flask, request, jsonify, make_response
-
+from trlx.data.configs import TRLConfig
 
 app = Flask(__name__)
 
+REWARD_MODELS = {
+    "openai_simple_labels": "reward/outputs/openai_simple_labels/checkpoint-1000/model.safetensors",
+    "openai_unbiased_labels": "reward/outputs/openai_unbiased_labels/checkpoint-700/model.safetensors",
+    "human_labels": "reward/outputs/human_labels/checkpoint-700/model.safetensors",
+}
 
 SFT_MODEL_PATH = "meta-llama/Llama-2-13b-hf"
-REWARD_CHECKPOINT_PATH = "reward/outputs/checkpoint-700/model.safetensors"
-tokenizer_path = "meta-llama/Llama-2-13b-hf"
+
+TOKENIZER_PATH = "meta-llama/Llama-2-13b-hf"
 
 
-rw_tokenizer = AutoTokenizer.from_pretrained(tokenizer_path, use_fast=False)
-if rw_tokenizer.pad_token is None:
-    rw_tokenizer.pad_token = rw_tokenizer.unk_token
-    print("set pad token to unk token: ", rw_tokenizer.pad_token)
-rw_model = GPTRewardModel(SFT_MODEL_PATH, tokenizer_path)
-print("Loading weights")
-rw_model.load_state_dict(safetensors.torch.load_file(REWARD_CHECKPOINT_PATH))
-rw_model.half()
-rw_model.eval()
-rw_device = torch.device("cuda:{}".format(0))
-rw_model.to(rw_device)
-print("reward model loaded!")
+def setup_reward_model(tokenizer_path, model_path, checkpoint_path):
+    global rw_tokenizer, rw_model, rw_device
+    
+    rw_tokenizer = AutoTokenizer.from_pretrained(tokenizer_path, use_fast=False)
+    if rw_tokenizer.pad_token is None:
+        rw_tokenizer.pad_token = rw_tokenizer.unk_token
+        print("set pad token to unk token: ", rw_tokenizer.pad_token)
+    
+    rw_model = GPTRewardModel(model_path, tokenizer_path)
+    print("Loading weights")
+    rw_model.load_state_dict(safetensors.torch.load_file(checkpoint_path))
+    rw_model.half()
+    rw_model.eval()
+    rw_device = torch.device("cuda:{}".format(0))
+    rw_model.to(rw_device)
+    print("reward model loaded!")
 
 
 def get_scores(samples: List[str]):
@@ -69,4 +78,13 @@ def get_reward():
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--reward_model", type=str, required=True, help="The reward model to use", choices=REWARD_MODELS.keys())
+    args = parser.parse_args()
+    
+    REWARD_CHECKPOINT_PATH = REWARD_MODELS[args.reward_model]
+    
+    setup_reward_model(TOKENIZER_PATH, SFT_MODEL_PATH, REWARD_CHECKPOINT_PATH)
+    
     app.run(debug=False, host="0.0.0.0", port=8115)
+
