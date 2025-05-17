180333ac999fc3362761ec2968e38d1023ac77c8
Lukas Fluri <lukas.fluri@protonmail.com>
2025-05-17 08:15:29 -0700

    Improved the agent evaluation logic.

--- experiments/qa/agent_eval.py
+++ experiments/qa/agent_eval.py
@@ -1,9 +1,14 @@
+import argparse
 import os
 import pathlib
 from datetime import datetime
 from typing import Callable, Dict, List, Optional
 
-from agent_train import DATA_PATH, QADataset, build_metric_fn, build_reward_fn
+import torch
+from agent_train import build_metric_fn, build_reward_fn
+from huggingface_hub import HfApi
+from qa_dataset import QADataset
+from tqdm import tqdm
 from transformers import AutoTokenizer
 
 from trlx.data.configs import TRLConfig
@@ -14,6 +19,8 @@ from trlx.trainer.nn.ppo_models import (
 )
 from trlx.utils import set_seed
 from trlx.utils.loading import get_pipeline, get_trainer
+
+CURRENT_DIR = pathlib.Path(__file__).parent
 
 
 def build_trainer_for_eval(
@@ -50,10 +57,24 @@ def build_trainer_for_eval(
             return Seq2SeqLMHydraWithValueHead(
                 config.model.model_path, config.model.num_layers_unfrozen
             )
+
+        # Check whether the provided model path is a hf repo
+        hf_api = HfApi()
+        if hf_api.repo_exists(repo_id=config.model.model_path, repo_type="model"):
+            # Create a name for the local directory
+            local_dir = (
+                CURRENT_DIR
+                / "model_checkpoints/PPO"
+                / config.model.model_path.replace("/", "_")
+            ).absolute()
+        else:
+            local_dir = None
+
         return CausalLMHydraWithValueHead.from_pretrained(
             config.model.model_path,
             model_architecture,
             num_layers_unfrozen=config.model.num_layers_unfrozen,
+            hf_local_target_dir=local_dir,
         )
 
     trainer_class = get_trainer(config.train.trainer)
@@ -120,26 +141,27 @@ def build_trainer_for_eval(
     return trainer
 
 
-if __name__ == "__main__":
-    # Load the config
-    config_path = pathlib.Path(__file__).parent.joinpath("configs/ppo_config_eval.yml")
-    config = TRLConfig.load_yaml(config_path)
+def evaluate_agent_with_trainer_pipeline(
+    config: TRLConfig,
+    qa_dataset: QADataset,
+    compute_reward_model_scores: bool = False,
+    model_architecture: str = "meta-llama/Llama-2-7b-hf",
+) -> Dict[str, List[float]]:
+    """
+    Evaluates the agent by using the trainer pipeline of the 'trlx' library that has been customized by the authors of the original codebase.
 
-    # Append the current timestamp to the checkpoint directory
-    config.train.checkpoint_dir = (
-        f"{config.train.checkpoint_dir}/{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
-    )
+    Note: If 'compute_reward_model_scores' is set to True, it is assumed that the reward model has been started as a separate process and is listening on the correct port.
 
-    # Build the dataset
-    # train_path = f"{DATA_PATH}/train_qa.json"
-    # test_path = f"{DATA_PATH}/val_qa.json"
-    train_path = f"{DATA_PATH}/train_qa_le8000.json"
-    test_path = f"{DATA_PATH}/val_qa_le8000.json"
-    qa_dataset = QADataset(train_path, test_path)
-
+    Args:
+        config (TRLConfig): The configuration object
+        qa_dataset (QADataset): The dataset object
+        compute_reward_model_scores (bool): Whether to compute reward model scores
+    Returns:
+        Dict[str, List[float]]: A dictionary containing the evaluation results
+    """
     # Build the prompts
     tokenizer = AutoTokenizer.from_pretrained(config.tokenizer.tokenizer_path)
-    val_prompts = [
+    evaluation_prompts = [
         item.build_prompt_for_agent(tokenizer)
         for item in qa_dataset
         if not item.is_train
@@ -148,16 +170,151 @@ if __name__ == "__main__":
     # Load the trainer from a checkpoint
     trainer = build_trainer_for_eval(
         config=config,
-        reward_fn=build_reward_fn(qa_dataset),
-        metric_fn=build_metric_fn(qa_dataset),
-        eval_prompts=val_prompts,  # Typically, eval_prompts can be the same as prompts for evaluation
+        reward_fn=build_reward_fn(
+            dataset=qa_dataset,
+            tokenizer=tokenizer,
+            skip_start_and_end_tokens=True,
+            compute_reward_model_scores=compute_reward_model_scores,
+        ),
+        metric_fn=build_metric_fn(
+            dataset=qa_dataset,
+            tokenizer=tokenizer,
+            skip_start_and_end_tokens=True,
+            compute_reward_model_scores=compute_reward_model_scores,
+        ),
+        eval_prompts=evaluation_prompts,  # Typically, eval_prompts can be the same as prompts for evaluation
+        model_architecture=model_architecture,
     )
 
-    # Perform evaluation
     evaluation_results = trainer.evaluate()
     date_time = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
     date_time_integer = int(date_time)
     trainer.accelerator.log(evaluation_results, step=date_time_integer)
+
+    return evaluation_results
+
+
+def evaluate_agent_manually(
+    config: TRLConfig,
+    qa_dataset: QADataset,
+    compute_reward_model_scores: bool = False,
+    model_architecture: str = "meta-llama/Llama-2-7b-hf",
+) -> Dict[str, List[float]]:
+    # Check whether the provided model path is a hf repo
+    hf_api = HfApi()
+    if hf_api.repo_exists(repo_id=config.model.model_path, repo_type="model"):
+        # Create a name for the local directory
+        local_dir = (
+            CURRENT_DIR
+            / "model_checkpoints/PPO"
+            / config.model.model_path.replace("/", "_")
+        ).absolute()
+    else:
+        local_dir = None
+
+    # Load the model
+    model = CausalLMHydraWithValueHead.from_pretrained(
+        config.model.model_path,
+        model_architecture,
+        num_layers_unfrozen=config.model.num_layers_unfrozen,
+        hf_local_target_dir=local_dir,
+    )
+
+    # Build the prompts
+    tokenizer = AutoTokenizer.from_pretrained(config.tokenizer.tokenizer_path)
+    evaluation_prompts = [
+        item.build_prompt_for_agent(tokenizer)
+        for item in qa_dataset
+        if not item.is_train
+    ]
+    evaluation_prompts = evaluation_prompts[:10]
+
+    max_prompt_length = (
+        config.train.seq_length - config.method.gen_kwargs["max_new_tokens"]
+    )
+
+    # Generate the responses
+    responses = []
+    with torch.no_grad():
+        for prompt in tqdm(evaluation_prompts, desc="Generating responses"):
+            model_inputs = tokenizer(
+                prompt,
+                truncation=True,
+                padding=False,
+                max_length=max_prompt_length,
+                add_special_tokens=True,
+                return_tensors="pt",
+            )
+
+            responses.append(
+                model.generate(
+                    input_ids=model_inputs["input_ids"].to(model.device),
+                    attention_mask=model_inputs["attention_mask"].to(model.device),
+                    **config.method.gen_kwargs,
+                    eos_token_id=tokenizer.eos_token_id,
+                    pad_token_id=tokenizer.pad_token_id,
+                )
+            )
+
+    # Compute the metrics
+    metric_function = build_metric_fn(
+        dataset=qa_dataset,
+        tokenizer=tokenizer,
+        skip_start_and_end_tokens=True,
+        compute_reward_model_scores=compute_reward_model_scores,
+    )
+    metrics = metric_function(responses)
+
+    return metrics
+
+
+if __name__ == "__main__":
+    # Setup command-line argument parsing
+    parser = argparse.ArgumentParser(description="Evaluate RL agent on QA tasks")
+
+    # fmt: off
+    parser.add_argument("--legacy_format", action="store_true", default=False, help="Use legacy data format and model configuration")
+    parser.add_argument("--compute_reward_model_scores", action="store_true", default=False, help="Compute reward model scores (requires reward model server)")
+    parser.add_argument("--model_architecture", type=str, help="The name of the underlying architecture of the model under evaluation. Should correspond to a model on Hugging Face.")
+    parser.add_argument("--use_trainer_evaluation", action="store_true", default=False, help="Use trainer evaluation pipeline instead of manual evaluation")
+    parser.add_argument("--config_path", type=str, help="Path to custom config file.")
+    parser.add_argument("--train_path", type=str, help="Path to training data.")
+    parser.add_argument("--validation_path", type=str, help="Path to validation data.")
+    # fmt: on
+
+    args = parser.parse_args()
+
+    # Set configuration based on command-line arguments
+    config_path = args.config_path
+
+    # Load the config
+    config = TRLConfig.load_yaml(config_path)
+
+    # Append the current timestamp to the checkpoint directory
+    config.train.checkpoint_dir = (
+        f"{config.train.checkpoint_dir}/{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
+    )
+
+    # Build the dataset
+    qa_dataset = QADataset(
+        args.train_path, args.validation_path, use_legacy_format=args.legacy_format
+    )
+
+    # Perform evaluation
+    if args.use_trainer_evaluation:
+        evaluation_results = evaluate_agent_with_trainer_pipeline(
+            config,
+            qa_dataset,
+            args.compute_reward_model_scores,
+            args.model_architecture,
+        )
+    else:
+        evaluation_results = evaluate_agent_manually(
+            config,
+            qa_dataset,
+            args.compute_reward_model_scores,
+            args.model_architecture,
+        )
 
     # Print evaluation metrics
     print("Evaluation Results:")
--- experiments/qa/scripts/agent_eval.sh
+++ experiments/qa/scripts/agent_eval.sh
@@ -12,12 +12,31 @@ GPU_IDS="0,1,2,3,4,5,6,7"  # You can modify this to use different GPUs, e.g. "0"
 # Calculate number of processes based on number of GPUs
 NUM_PROCESSES=$(echo $GPU_IDS | tr ',' '\n' | wc -l)
 
-CONFIG_FILE="configs/default_accelerate_config_final.yaml"
-CONFIG_PATH_TRLX="configs/ppo_config_eval.yml"
+CONFIG_FILE_ACCELERATE="configs/default_accelerate_config_final.yaml"
+
+# Build all arguments
+USE_LEGACY_FORMAT=true
+COMPUTE_REWARD_MODEL_SCORES=false
+USE_TRAINER_EVALUATION=true
+MODEL_ARCHITECTURE="meta-llama/Llama-2-7b-hf"
+TRAIN_PATH="../data/qa/train_qa.json"
+VALIDATION_PATH="../data/qa/val_qa.json"
+CONFIG_FILE_TRLX="../configs/ppo_config_eval_legacy.yml"
+
+ARGS="--config_path $CONFIG_FILE_TRLX --train_path $TRAIN_PATH --validation_path $VALIDATION_PATH --model_architecture $MODEL_ARCHITECTURE"
+if [ "$USE_LEGACY_FORMAT" = true ]; then
+    ARGS="$ARGS --legacy_format"
+fi
+if [ "$COMPUTE_REWARD_MODEL_SCORES" = true ]; then
+    ARGS="$ARGS --compute_reward_model_scores"
+fi
+if [ "$USE_TRAINER_EVALUATION" = true ]; then
+    ARGS="$ARGS --use_trainer_evaluation"
+fi
 
 accelerate launch \
     --main_process_port 25913 \
     --num_processes $NUM_PROCESSES \
     --gpu_ids $GPU_IDS \
-    --config_file $CONFIG_FILE \
-    eval.py --config_path $CONFIG_PATH_TRLX
+    --config_file $CONFIG_FILE_ACCELERATE \
+    ../agent_eval.py $ARGS
